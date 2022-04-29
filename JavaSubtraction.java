5cb9b272e13dd5368bf910f4bc4637a0b1d93bdc
RoshanM24 <roshanmanglani24@gmail.com>
2022-04-29 10:07:18 +0530

    Add

---
+++ JavaSubtraction.java
@@ -0,0 +1,10 @@
+public class JavaSubtraction
+{  
+   public static void main(String args[])   
+ {  
+	int n1=Integer.parseInt(args[0]), n2=Integer.parseInt(args[1]), sub; 
+	System.out.println(n1 +" "+ n2); 
+	sub = n1 - n2;  
+	System.out.println("The subtraction of two numbers is: "+sub);  
+ }  
+}
